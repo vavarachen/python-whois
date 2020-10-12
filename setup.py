6c6d6d09b413b56622cbb00aea67fb349d199b5a
koravx <Vijay.Kora@abbvie.com>
2020-10-12 16:35:50 -0500

    Code Cleanup - PEP 8 - Removed Python2 compatibility code - updated countries.dat - version bump

--- setup.py
+++ setup.py
@@ -1,16 +1,28 @@
 from setuptools import setup
 
 setup(name='pythonwhois',
-      version='2.4.3',
-      description='Module for retrieving and parsing the WHOIS data for a domain. Supports most domains. No dependencies.',
+      version='2.4.4',
+      description='Module for retrieving and parsing WHOIS data for domains. Supports most domains. No dependencies.',
       author='Sven Slootweg',
       author_email='pythonwhois@cryto.net',
       url='http://cryto.net/pythonwhois',
       packages=['pythonwhois'],
-      package_dir={"pythonwhois":"pythonwhois"},
-      package_data={"pythonwhois":["*.dat"]},
+      package_dir={"pythonwhois": "pythonwhois"},
+      package_data={"pythonwhois": ["*.dat"]},
       install_requires=['argparse'],
       provides=['pythonwhois'],
       scripts=["pwhois"],
-      license="WTFPL"
-     )
+      license="WTFPL",
+      classifiers=[
+            'Development Status :: 1 - Beta',
+            'Intended Audience :: Developers',
+            'Natural Language :: English',
+            'Operating System :: Microsoft',
+            'Operating System :: POSIX :: Linux',
+            'Operating System :: MacOS :: MacOS X',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 3.6',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'Topic :: Software Development :: User Interfaces'
+      ]
+)
